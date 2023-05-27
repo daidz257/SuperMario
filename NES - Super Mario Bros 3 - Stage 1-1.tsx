d874b2bfc826a037cf491ffc1b12986c3e65206f
daidz257 <daidz257@gmail.com>
2023-05-27 16:35:27 +0700

    Adding files

---
+++ NES - Super Mario Bros 3 - Stage 1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="NES - Super Mario Bros 3 - Stage 1-1" tilewidth="16" tileheight="16" tilecount="6864" columns="176">
+ <image source="NES - Super Mario Bros 3 - Stage 1-1.png" width="2816" height="626"/>
+</tileset>
---
+++ NES - Super Mario Bros 3 - Stage Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="NES - Super Mario Bros 3 - Stage Tiles" tilewidth="16" tileheight="16" spacing="1" margin="2" tilecount="1533" columns="73">
+ <image source="NES - Super Mario Bros 3 - Stage Tiles.png" width="1242" height="358"/>
+</tileset>
